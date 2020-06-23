8ebc78c7ced73b7da9e3db329876fc6a4a4d3bb1
hsytkm <t_hosoya1214@yahoo.co.jp>
2020-06-23 22:56:57 +0900

    Add load thumbnail

---
+++ WpfControlSamples/Extensions/BitmapSourceExtension.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WpfControlSamples.Extensions
+{
+    static class BitmapSourceExtension
+    {
+        /// <summary>
+        /// 引数PATHの画像を埋め込みサムネイル優先で読み出す
+        /// </summary>
+        /// <param name="imagePath">ファイルパス</param>
+        /// <param name="widthMax">埋め込みサムネイルが存在しなかった場合に、主画像をリサイズする最大幅</param>
+        /// <returns></returns>
+        public static BitmapSource FilePathToBitmapSourceFromEmbeddedThumbnail(this string imagePath, double widthMax)
+        {
+            if (!File.Exists(imagePath)) throw new FileNotFoundException(imagePath);
+
+            using var stream = File.Open(imagePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            return stream.ToBitmapSourceFromEmbeddedThumbnail(widthMax);
+        }
+
+        /// <summary>
+        /// 画像のStreamから埋め込みサムネイル優先でBitmapSourceを作成する
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="widthMax">埋め込みサムネイルが存在しなかった場合に、主画像をリサイズする最大幅</param>
+        /// <returns></returns>
+        private static BitmapSource ToBitmapSourceFromEmbeddedThumbnail(this Stream stream, double widthMax)
+        {
+            if (stream is null) throw new ArgumentNullException(nameof(stream));
+            var image = BitmapFrame.Create(stream, BitmapCreateOptions.None, BitmapCacheOption.None);
+
+            BitmapSource bitmap;
+            if (image.Thumbnail != null)
+            {
+                bitmap = image.Thumbnail;
+            }
+            else
+            {
+                var longSide = Math.Max(image.PixelWidth, image.PixelHeight);
+                if (longSide == 0) throw new DivideByZeroException(nameof(longSide));
+                var scale = widthMax / (double)longSide;
+                var thumbnail = new TransformedBitmap(image, new ScaleTransform(scale, scale));
+                var cache = new CachedBitmap(thumbnail, BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
+                cache.Freeze();
+                bitmap = (BitmapSource)cache;  // アップキャストは常に合法
+            }
+            return bitmap;
+        }
+
+    }
+}
---
+++ WpfControlSamples/Views/Menus/Presentation/ImageThumbnailPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfControlSamples.Extensions;
+using WpfControlSamples.Infrastructures;
+
+namespace WpfControlSamples.Views.Menus
+{
+    public partial class ImageThumbnailPage : ContentControl
+    {
+        public ImageThumbnailPage()
+        {
+            InitializeComponent();
+            DataContext = new ImageThumbnailViewModel();
+        }
+    }
+
+    class ImageThumbnailViewModel : MyBindableBase
+    {
+        // 埋め込みサムネイルが存在しなかった場合に、主画像をリサイズする最大幅
+        // (埋め込みサムネイルが存在しなかったことを分かりやすいように大きくしとく）
+        private const double _thumbnailImageWidthMax = 1280;
+
+        public string SelectedOpenPath
+        {
+            get => _selectedOpenPath;
+            set
+            {
+                if (SetProperty(ref _selectedOpenPath, value))
+                {
+                    SelectedImage = value?.FilePathToBitmapSourceFromEmbeddedThumbnail(_thumbnailImageWidthMax);
+                }
+            }
+        }
+        private string _selectedOpenPath;
+
+        public BitmapSource SelectedImage
+        {
+            get => _selectedImage;
+            private set
+            {
+                if (SetProperty(ref _selectedImage, value))
+                {
+                    ImageSize = value is null ? new Size() : new Size(value.PixelWidth, value.PixelHeight);
+                }
+            }
+        }
+        private BitmapSource _selectedImage;
+
+        public Size ImageSize
+        {
+            get => _imageSize;
+            private set => SetProperty(ref _imageSize, value);
+        }
+        private Size _imageSize;
+    }
+}
