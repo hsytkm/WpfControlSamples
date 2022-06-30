d08e41b9573afd572dbe8c487523f968d8cbc66d
myoga <34601860+hsytkm@users.noreply.github.com>
2022-06-30 22:12:59 +0900

    capture key down event

--- WpfControlSamples/Views/Menus/InteractionTriggers/KeyTriggerPage.xaml.cs
+++ WpfControlSamples/Views/Menus/InteractionTriggers/KeyTriggerPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿#nullable disable
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Diagnostics;
@@ -21,11 +20,44 @@ namespace WpfControlSamples.Views.Menus
 {
     public partial class KeyTriggerPage : ContentControl
     {
+        public static readonly DependencyProperty KeyTextProperty =
+            DependencyProperty.Register(nameof(KeyText), typeof(string), typeof(KeyTriggerPage));
+        public string KeyText
+        {
+            get => (string)GetValue(KeyTextProperty);
+            set => SetValue(KeyTextProperty, value);
+        }
+
         public KeyTriggerPage()
         {
-            InitializeComponent();
             DataContext = new KeyTriggerViewModel();
+            InitializeComponent();
+
+            Loaded += KeyTriggerPage_Loaded;
+            Unloaded += KeyTriggerPage_Unloaded;
         }
+
+        //[How can I capture KeyDown event on a WPF Page or UserControl object? - Stack Overflow](https://stackoverflow.com/questions/347724/how-can-i-capture-keydown-event-on-a-wpf-page-or-usercontrol-object)
+        private Window? _window;
+
+        private void KeyTriggerPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= KeyTriggerPage_Loaded;
+
+            _window = Window.GetWindow(this);
+            if (_window is not null)
+                _window.KeyDown += Window_KeyDown;
+        }
+
+        private void KeyTriggerPage_Unloaded(object sender, RoutedEventArgs e)
+        {
+            Unloaded -= KeyTriggerPage_Unloaded;
+
+            if (_window is not null)
+                _window.KeyDown -= Window_KeyDown;
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e) => KeyText = e.Key.ToString();
     }
 
     class KeyTriggerViewModel : MyBindableBase
@@ -37,11 +69,10 @@ namespace WpfControlSamples.Views.Menus
         }
         private string _message = "Push any key.";
 
-        public ICommand PushKeyCommand => _pushKeyCommand ??
-            (_pushKeyCommand = new MyCommand<object>(x => Message = x.ToString()));
-        private ICommand _pushKeyCommand;
+        public ICommand PushKeyCommand => _pushKeyCommand ??=
+            new MyCommand<object>(x => Message = x?.ToString() ?? "");
+        private ICommand? _pushKeyCommand;
 
         public void PushKeyF1() => Message = nameof(PushKeyF1);
-
     }
 }
