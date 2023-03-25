e5dff88a0c3124b9253ad126f26abc37bac884a9
myoga <34601860+hsytkm@users.noreply.github.com>
2023-03-25 13:07:15 +0900

    add props

--- WpfControlSamples/App.xaml.cs
+++ WpfControlSamples/App.xaml.cs
@@ -1,42 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Threading;
 using WpfControlSamples.Models;
 
-namespace WpfControlSamples
+namespace WpfControlSamples;
+
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    // AppクラスにModelインスタンスを持たせる
+    // https://github.com/runceel/Livet-samples/blob/master/CommunicationBetweenViewModels/App.xaml.cs
+    public static new App Current => (App)Application.Current;
+
+    internal SampleModelContext SampleModelContext { get; } = new();
+
+    private void App_OnStartup(object sender, StartupEventArgs e)
     {
-        // AppクラスにModelインスタンスを持たせる
-        // https://github.com/runceel/Livet-samples/blob/master/CommunicationBetweenViewModels/App.xaml.cs
-        public static new App Current => (App)Application.Current;
+        // App内で MainWindow のインスタンスを取得するためxamlで直設定しています
+        MainWindow.Closing += static (_, _) => Debug.WriteLine("Call Dispose() here.");
 
-        internal SampleModelContext SampleModelContext { get; } = new SampleModelContext();
-
-        #region DispatcherUnhandledException
-        // ここで例外をハンドルしなければアプリ死にます
-        // http://gushwell.ldblog.jp/archives/52335498.html
-        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
-        {
-            var message = $"未ハンドルの例外を検出しましたー" + Environment.NewLine
-                + $"{e.Exception.GetType()} : {e.Exception.Message}";
-
-            MessageBox.Show(message, "Exception occurred", MessageBoxButton.OK, MessageBoxImage.Error);
-            e.Handled = true;
-        }
-
-        public void RemoveEvent_DispatcherUnhandledException()
-            => DispatcherUnhandledException -= Application_DispatcherUnhandledException;
-        #endregion
-
+        // https://zenn.dev/nuits_jp/articles/2023-03-08-wpf-unhandled-exception
     }
+
+    #region DispatcherUnhandledException
+    // ここで例外をハンドルしなければアプリ死にます
+    // http://gushwell.ldblog.jp/archives/52335498.html
+    private void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+        var message = $"未ハンドルの例外を検出しましたー" + Environment.NewLine
+            + $"{e.Exception.GetType()} : {e.Exception.Message}";
+
+        MessageBox.Show(message, "Exception occurred", MessageBoxButton.OK, MessageBoxImage.Error);
+        e.Handled = true;
+    }
+
+    public void RemoveEvent_DispatcherUnhandledException()
+        => DispatcherUnhandledException -= App_DispatcherUnhandledException;
+    #endregion
 }
